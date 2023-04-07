c71a7167fb1e34fc96c07414446f1209a66c4ff5
Minilik94 <minilikzeru@gmail.com>
2023-04-07 17:31:52 +0300

    Full-Stack Random Ideas App

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const ideasRouter = require("./routes/ideas");
 const connectDB = require('./config/db')
@@ -7,6 +8,10 @@ const port = process.env.PORT || 5000;
 connectDB()
 
 const app = express();
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
